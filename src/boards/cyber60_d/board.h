169c01025561de916c6d1c188b793ad3e9aea6e2
4pplet <mail@4pplet.com>
2022-03-21 11:50:16 +0100

    test

--- src/boards/cyber60_d/board.h
+++ src/boards/cyber60_d/board.h
@@ -31,41 +31,39 @@
 /* LED
  *------------------------------------------------------------------*/
 #define LEDS_NUMBER           2
-#define LED_PRIMARY_PIN       _PINNUM(0, 5)  // Red
-#define LED_SECONDARY_PIN     _PINNUM(0, 7) // Green
-#define LED_STATE_ON          0
+#define LED_PRIMARY_PIN       _PINNUM(1, 9)
+#define LED_SECONDARY_PIN     _PINNUM(1, 10)
+#define LED_STATE_ON          1
 
-#define LED_RGB_RED_PIN       _PINNUM(0, 5)
-#define LED_RGB_GREEN_PIN     _PINNUM(0, 7)
-#define LED_RGB_BLUE_PIN      _PINNUM(1, 9)
-#define BOARD_RGB_BRIGHTNESS  0x404040
+#define LED_NEOPIXEL           _PINNUM(0, 16)
+#define NEOPIXELS_NUMBER      1
+#define BOARD_RGB_BRIGHTNESS  0x040404
 
 /*------------------------------------------------------------------*/
 /* BUTTON
  *------------------------------------------------------------------*/
-#define BUTTONS_NUMBER      2
-#define BUTTON_1            11
-#define BUTTON_2            12
-#define BUTTON_PULL         NRF_GPIO_PIN_PULLUP
+#define BUTTONS_NUMBER        2
+#define BUTTON_1              _PINNUM(1, 02)
+#define BUTTON_2              _PINNUM(0, 10)
+#define BUTTON_PULL           NRF_GPIO_PIN_PULLUP
 
 //--------------------------------------------------------------------+
 // BLE OTA
 //--------------------------------------------------------------------+
-#define BLEDIS_MANUFACTURER    "4pplet"
-#define BLEDIS_MODEL           "cyber60 D"
+#define BLEDIS_MANUFACTURER   "Adafruit Industries"
+#define BLEDIS_MODEL          "Feather nRF52840 Sense"
 
 //--------------------------------------------------------------------+
 // USB
 //--------------------------------------------------------------------+
-
-// Shared VID/PID with Feather nRF52840, will be disabled for building in the future
 #define USB_DESC_VID           0x239A
-#define USB_DESC_UF2_PID       0x0029
-#define USB_DESC_CDC_ONLY_PID  0x0029
+#define USB_DESC_UF2_PID       0x0087
+#define USB_DESC_CDC_ONLY_PID  0x0088
 
-#define UF2_PRODUCT_NAME    "cyber60 Rev D"
-#define UF2_VOLUME_LABEL    "CYBER60_D"
-#define UF2_BOARD_ID        "nRF52840-cyber60-rev_d"
-#define UF2_INDEX_URL       "https://github.com/4pplet/cyber60"
+//------------- UF2 -------------//
+#define UF2_PRODUCT_NAME      "Adafruit Feather nRF52840 Sense"
+#define UF2_VOLUME_LABEL      "FTHRSNSBOOT"
+#define UF2_BOARD_ID          "nRF52840-Feather-Sense"
+#define UF2_INDEX_URL         "https://www.adafruit.com/product/4516"
 
-#endif // CYBER60_V_H
+#endif // _FEATHER_NRF52840_SENSE_H
